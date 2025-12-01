a0474063dd35ee0ef949793e22908810d8ffbda5
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-12-01 16:24:35 +0530

    route element

--- dashboard-x/src/App.jsx
+++ dashboard-x/src/App.jsx
@@ -1,23 +1,23 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import RequireAuth from './components/RequireAuth';
-import DashboardLayout from './components/DashboardLayout';
-import Login from './pages/Login';
-import Overview from './pages/Overview';
-import Activity from './pages/Activity';
-import Command from './pages/Command';
-import Users from './pages/Users';
-import Build from './pages/Build';
-import SelfDesign from './pages/SelfDesign';
-import UniversalStoreIntegration from './pages/UniversalStoreIntegration';
-import PageBuilder from './pages/PageBuilder';
-import Joe from './pages/Joe';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import RequireAuth from "./components/RequireAuth";
+import DashboardLayout from "./components/DashboardLayout";
+import Login from "./pages/Login";
+import Overview from "./pages/Overview";
+import Activity from "./pages/Activity";
+import Command from "./pages/Command";
+import Users from "./pages/Users";
+import Build from "./pages/Build";
+import SelfDesign from "./pages/SelfDesign";
+import UniversalStoreIntegration from "./pages/UniversalStoreIntegration";
+import PageBuilder from "./pages/PageBuilder";
+import Joe from "./pages/Joe";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        
+
         <Route
           path="/"
           element={
@@ -33,7 +33,10 @@ function App() {
           <Route path="users" element={<Users />} />
           <Route path="build" element={<Build />} />
           <Route path="self-design" element={<SelfDesign />} />
-          <Route path="store-integration" element={<UniversalStoreIntegration />} />
+          <Route
+            path="store-integration"
+            element={<UniversalStoreIntegration />}
+          />
           <Route path="page-builder" element={<PageBuilder />} />
           <Route path="joe" element={<Joe />} />
         </Route>
